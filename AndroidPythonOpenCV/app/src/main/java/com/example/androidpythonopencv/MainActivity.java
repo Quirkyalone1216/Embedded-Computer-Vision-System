8b2e930f2fcfedb8b3f787328f26b7721ec5eed1
Quirkyalone1216 <chen.ken1216@gmail.com>
2025-03-26 11:03:53 +0800

    2023.03.26 上課內容更新

--- AndroidPythonOpenCV/app/src/main/java/com/example/androidpythonopencv/MainActivity.java
+++ AndroidPythonOpenCV/app/src/main/java/com/example/androidpythonopencv/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.androidpythonopencv;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,49 +20,58 @@ import com.chaquo.python.Python;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.android.AndroidPlatform;
 
+import java.io.ByteArrayOutputStream;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    Button Go_btn;
+    ImageView src_image, res_image;
+
+    BitmapDrawable drawable;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Go_btn = findViewById(R.id.Go_button);
+        src_image = (ImageView) findViewById(R.id.source_imageview);
+        res_image = (ImageView) findViewById(R.id.response_imageview);
         //初始化python环境
-        if(!Python.isStarted()){
+        if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
+        Python python_cv = Python.getInstance();
 
+        Go_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 获取源图片并转换为Bitmap对象
+                drawable = (BitmapDrawable) src_image.getDrawable();
+                bitmap = drawable.getBitmap();
+                // 将Bitmap转换为byte[]对象
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
 
-        Log.d("Python", "Python initialized");
+                // 调用Python方法处理图片
+                PyObject cvObject = python_cv.getModule("opencv_python");
+                byte[] bytes = cvObject.callAttr("opencv_process_image", byteArray).toJava(byte[].class);
 
-
-        //使用numpy计算两个矩阵的乘积
-        Python py = Python.getInstance();
-
-        //调用hello_python.py里面的matrix_multiply函式
-        PyObject pyObj = py.getModule("hello_python").get("matrix_multiply");
-
-
-        //将matrix_multiply计算完的数值，换成java中的float类型
-
-        float[][] result = pyObj.call().toJava(float[][].class);
-        String resultStr = "";
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
-                resultStr += result[i][j] + " ";
+                // 将处理后的图片显示到画面上
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                res_image.setImageBitmap(bmp);
             }
-            resultStr += "\n";
-
-        }
-        Log.d("Result", resultStr);
-        Log.d("Python", "Result logged: " + resultStr);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
         });
     }
 }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Log.d("DEBUG", "HELLO WORLD layout loaded");
+//    }
